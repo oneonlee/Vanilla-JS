ea297d20fcbc5192c4d5bba82cdc768e30f13b78
Lee Dong-geon <peter8739@gmail.com>
2021-02-16 00:39:12 +0900

    Update index.js

--- 05. Xmas Countdown/src/index.js
+++ 05. Xmas Countdown/src/index.js
@@ -1,30 +1,27 @@
-/* You're gonna need this
-const NINE_HOURS_MILLISECONDS = 32400000; */
+import "./styles.css";
 
 const timer = document.querySelector("h2");
 
+// You're gonna need this
 function getTime() {
   // Don't delete this.
-  var currentDay = new Date();
-
   const xmasDay = new Date("2021-12-24:00:00:00+0900");
-  var gap = xmasDay.getTime() - currentDay.getTime(); // 경과 시간, ms 단위
+  var currentTime = new Date();
+  var timeDifference = xmasDay - currentTime;
 
-  var transDay = parseInt(gap / (1000 * 60 * 60 * 24), 10); // 경과시간 중 일 단위
-  var transHour = parseInt(gap / (1000 * 60 * 60), 10) % 24; // 경과시간 중 시 단위
-  var transMin = parseInt(gap / (1000 * 60), 10) % 60; // 경과시간 중 분 단위
-  var transSec = parseInt(gap / 1000, 10) % 60; // 경과시간 중 초 단위
+  var msToDay = parseInt(timeDifference / (1000 * 60 * 60 * 24), 10); // 경과시간 중 일 단위
+  var msToHour = parseInt(timeDifference / (1000 * 60 * 60), 10) % 24; // 경과시간 중 시 단위
+  var msToMin = parseInt(timeDifference / (1000 * 60), 10) % 60; // 경과시간 중 분 단위
+  var msToSec = parseInt(timeDifference / 1000, 10) % 60; // 경과시간 중 초 단위
 
-  timer.innerText = `${transDay}d ${
-    transHour < 10 ? `0${transHour}` : transHour
-  }h ${transMin < 10 ? `0${transMin}` : transMin}m ${
-    transSec < 10 ? `0${transSec}` : transSec
+  timer.innerText = `${msToDay}d ${
+    msToHour < 10 ? `0${msToHour}` : msToHour
+  }h ${msToMin < 10 ? `0${msToMin}` : msToMin}m ${
+    msToSec < 10 ? `0${msToSec}` : msToSec
   }s`;
 }
 
 function init() {
-  getTime();
   setInterval(getTime, 500);
 }
-
 init();
