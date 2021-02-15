403f5df4baabc96d4504f31bdc5c6bee1660fc61
Lee Dong-geon <peter8739@gmail.com>
2021-02-16 00:46:00 +0900

    Update index.js

--- 05. Xmas Countdown/src/index.js
+++ 05. Xmas Countdown/src/index.js
@@ -5,7 +5,7 @@ const timer = document.querySelector("h2");
 // You're gonna need this
 function getTime() {
   // Don't delete this.
-  const xmasDay = new Date("2021-12-24:00:00:00+0900");
+  const xmasDay = new Date("2021-12-25:00:00:00+0900");
   var currentTime = new Date();
   var timeDifference = xmasDay - currentTime;
 
